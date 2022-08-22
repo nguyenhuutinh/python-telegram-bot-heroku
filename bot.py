ef335dff8a57b92480ea36fcfe2c9d5cd702350b
T Nguyen <nguyenhuutinh@users.noreply.github.com>
2022-08-23 00:51:36 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import logging
 from telegram import Chat, ChatMember, ChatMemberUpdated, Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ChatMemberHandler, ContextTypes
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters, ChatMemberHandler, ContextTypes
 from telegram import ParseMode
 from typing import Optional, Tuple
 
@@ -73,7 +73,7 @@ def main():
 
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(MessageHandler(filters.TEXT, echo))
 
     # log all errors
     dp.add_error_handler(error)
