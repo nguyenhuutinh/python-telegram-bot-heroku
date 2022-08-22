7c98f49e17d94930c88793c4242025df23bcebd5
T Nguyen <nguyenhuutinh@users.noreply.github.com>
2022-08-23 00:19:37 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import logging
 from telegram import Chat, ChatMember, ChatMemberUpdated, Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ChatMemberHandler
-from telegram.ext import ParseMode
+from telegram import ParseMode
 from typing import Optional, Tuple
 
 import os
